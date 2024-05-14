9c424dbca53eba5ea49ae3e22fef10de3294d4a2
bn-c <root.bachnc@gmail.com>
2024-05-14 15:35:50 +0900

    FIX drop rows in the dataset that are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,10 @@ def go(args):
     logger.info("Dropping duplicates")
     df = df.drop_duplicates().reset_index(drop=True)
 
+    # Drop rows with improper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save to csv file
     logger.info("Save to csv file")
     df.to_csv(args.output_artifact, index=False)
